9a57bf897e300661d0d34c930bfb30681edacbe0
Marco Arath <marco.martinez.morales@u-erre.mx>
2023-04-25 17:55:47 -0600

    se linkea la pagina mi nombre y se agrega el script para cambiar el nombre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,48 @@
-console.log('Hola desde un script externo');
+ 
+ console.log('Hola desde un script externo');
+
+function changeColorTo(color) {
+    const refName = document.getElementById('name');
+    refName.style.color = color;
+}
+
+function getReferenceFullName(){
+    return document.getElementById("fullname");
+}
+
+function changeColorParagraph(color, refObj) {
+    console.log(refObj);
+    refObj.style.color = color;
+}
+
+function resetColors(){
+    const refObjs = document.getElementsByClassName("text-color");
+    console.log(refObjs);
+    for (let index = 0; index < refObjs.length; index++) {
+        const element = refObjs[index];
+        element.style.color = "black";       
+    }
+}
+
+/* Función factible pero más larga porque es una función por cada botón*/
+function changeColorToBlue() {
+    const refName = document.getElementById("name");
+    refName.style.color = "blue";
+}
+
+function changeColorToGray() {
+    const refName = document.getElementById("name");
+    refName.style.color = "gray";
+}
 
 function changeColorToRed() {
-    const refName = document.getElementById('name');
-
-    refName.style.color = 'red';
+    const refName = document.getElementById("name");
+    refName.style.color = "red";
 }
+
+// Boton para cambiar nombre
+function changeName() {
+    const name = prompt("¿Cuál es tu nombre?");
+    const refName = document.getElementById("change-name");
+    refName.innerHTML="Hola " + name +", ¡Bienvenido!";
+  }
