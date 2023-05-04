fba998291931ac27c5a93316ca074d49a69e15b3
Marco Arath <marco.martinez.morales@u-erre.mx>
2023-05-04 15:17:46 -0600

    fechas.js

--- assets/js/fechas.js
+++ assets/js/fechas.js
@@ -9,10 +9,9 @@ function calcular() {
     
     const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
   
-    // Obtenemos el número del día de la semana (0 = domingo, 1 = lunes, etc.)
     const diaSemana = fecha.getDay();
   
-    // Definimos las opciones para el switch
+    //switch
     let resultado;
     switch (diaSemana) {
       case 0:
@@ -23,13 +22,13 @@ function calcular() {
         resultado = 'día laborable';
     }
   
-    // Verificamos si el año es bisiesto
+    //año bisiesto
     let esBisiesto = false;
     if (anio % 4 === 0 && (anio % 100 !== 0 || anio % 400 === 0)) {
       esBisiesto = true;
     }
   
-    // Mostramos el resultado en la página
+    //resultado en pagina
     const mensajeResultado = `${diasSemana[diaSemana]}, ${resultado}`;
     const mensajeBisiesto = esBisiesto ? 'El año es bisiesto' : 'El año no es bisiesto';
     document.getElementById('resultado').textContent = `${mensajeResultado}. ${mensajeBisiesto}.`;
